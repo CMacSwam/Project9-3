ab96f291bfd5aa8367c615f10bb92ce7dfc11018
CMacSwam <c-mccluskey.1@onu.edu>
2024-10-31 09:41:23 -0400

    Completed hasRun

--- Project9-3/Project9-3.cpp
+++ Project9-3/Project9-3.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int rollDie();
 void displayRun(int values[], int size);
+bool hasRun(int values[], int size);
 
 int main() {
 	srand(static_cast<unsigned int>(time(0)));
@@ -23,4 +24,20 @@ void displayRun(int values[], int size) {
 	}
 
 }
+
+bool hasRun(int values[], int size) {
+
+	bool run = false;
+
+	for (int i = 0; i < size - 1; i++) {
+
+		if (values[i] == values[i + 1]) {
+			run = true;
+		}
+	}
+
+	return run;
+}
+
+
 
