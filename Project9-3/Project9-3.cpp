cc1851494a347333efb5c8963bb2ec3b7f0bf56c
CMacSwam <c-mccluskey.1@onu.edu>
2024-10-31 09:38:37 -0400

    Completed displayRun

--- Project9-3/Project9-3.cpp
+++ Project9-3/Project9-3.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+int rollDie();
+void displayRun(int values[], int size);
+
 int main() {
+	srand(static_cast<unsigned int>(time(0)));
 
 }
+
+int rollDie() {
+	return rand() % 6 + 1; 
+}
+
+void displayRun(int values[], int size) {
+
+	for (int i = 0; i < size; i++) {
+		cout << values[i];
+	}
+
+}
+
